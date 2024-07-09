9695828e1b9300769c640b599845cd3413be62eb
Yura Kasyan <33.yurakasyan@gmail.com>
2024-07-09 15:11:00 +0200

    refactor: adds posibillity to fetch user's data when page has been reloaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/ui/Layout/Layout';
+import { useDispatch, useSelector } from 'react-redux';
 
+import Layout from './components/ui/Layout/Layout';
 import HomePage from './pages/HomePage/HomePage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ContactsPage from './pages/ContactsPage/ContactsPage';
 
+import { selectIsRefreshing } from './redux/auth/selectors';
+import { refreshUser } from './redux/auth/operations';
+
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <div>Refreshing user...</div>
+  ) : (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
