f4e5d5e26a2c7525abc7af207f83716b0b2ac3cb
Yura Kasyan <33.yurakasyan@gmail.com>
2024-07-28 23:19:28 +0200

    chore: installs Material UI library in project

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,56 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store.js';
 
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
+
 import App from './App.jsx';
 
 import './index.css';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#6358dc',
+      light: '#877ef1',
+      dark: '#413a94',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#f9329b',
+      light: '#ff67b7',
+      dark: '#b3236e',
+      contrastText: '#ffffff',
+    },
+    background: {
+      default: '#e0e0e0',
+      paper: '#eeeeee',
+    },
+    text: {
+      primary: 'rgba(0,0,0,0.92)',
+      secondary: 'rgba(0,0,0,0.6)',
+    },
+  },
+  typography: {
+    fontFamily: '"Poppins", "Roboto", "Helvetica", sans-serif',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <PersistGate loading={null} persistor={persistor}>
-          <App />
+          <CssBaseline />
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
         </PersistGate>
       </BrowserRouter>
     </Provider>
