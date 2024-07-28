94a0510428229c11cb126deaf2e3314398447f37
Yura Kasyan <33.yurakasyan@gmail.com>
2024-07-28 23:31:30 +0200

    refactor: makes changes in LoginPage and RegistrationPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,25 @@
+import AuthLabel from '../../components/ui/AuthLabel/AuthLabel';
 import AuthForm from '../../components/AuthForm/AuthForm';
+import OppositeOffer from '../../components/ui/OppositeOffer/OppositeOffer';
+import { useErrorMessage } from '../../helpers/hooks/useErrorMessage';
+
+import css from './LoginPage.module.css';
+import ErrorMessage from '../../components/ui/ErrorMessage/ErrorMessage';
 
 function LoginPage() {
-  return <AuthForm authType="Login" />;
+  const { errorMessage } = useErrorMessage();
+
+  return (
+    <section className={css.loginSection}>
+      <AuthLabel textLabel="Sign in" />
+
+      <AuthForm authType="Login" />
+
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+
+      <OppositeOffer isRegistered={false} />
+    </section>
+  );
 }
 
 export default LoginPage;
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,7 +1,17 @@
 import AuthForm from '../../components/AuthForm/AuthForm';
+import AuthLabel from '../../components/ui/AuthLabel/AuthLabel';
+import OppositeOffer from '../../components/ui/OppositeOffer/OppositeOffer';
+
+import css from './RegistrationPage.module.css';
 
 function RegistrationPage() {
-  return <AuthForm authType="Register" />;
+  return (
+    <section className={css.registerSection}>
+      <AuthLabel textLabel="Sign up" />
+      <AuthForm authType="Register" />
+      <OppositeOffer isRegistered={true} />
+    </section>
+  );
 }
 
 export default RegistrationPage;
