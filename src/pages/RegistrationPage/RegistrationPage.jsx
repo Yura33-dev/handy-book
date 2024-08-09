cdee4c8fddf26c45e81f584b9df13e5a30bc9ab8
Yura Kasyan <33.yurakasyan@gmail.com>
2024-08-09 18:32:08 +0200

    feature: provides ErrorMessage in RegistrationPage

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,14 +1,22 @@
 import AuthForm from '../../components/AuthForm/AuthForm';
 import AuthLabel from '../../components/ui/AuthLabel/AuthLabel';
+import ErrorMessage from '../../components/ui/ErrorMessage/ErrorMessage';
 import OppositeOffer from '../../components/ui/OppositeOffer/OppositeOffer';
+import { useErrorMessage } from '../../helpers/hooks/useErrorMessage';
 
 import css from './RegistrationPage.module.css';
 
 function RegistrationPage() {
+  const { errorMessage } = useErrorMessage();
+
   return (
     <section className={css.registerSection}>
       <AuthLabel textLabel="Sign up" />
+
       <AuthForm authType="Register" />
+
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+
       <OppositeOffer isRegistered={true} />
     </section>
   );
