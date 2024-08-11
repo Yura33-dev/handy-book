b2225320dfc26a70481b098dc0eec7541b8c2688
Yura Kasyan <33.yurakasyan@gmail.com>
2024-08-11 13:41:21 +0200

    refactor: changes error value in contact slice

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -9,6 +9,7 @@ import { logout } from '../auth/operations';
 
 const handlePending = state => {
   state.loading = true;
+  state.error = '';
 };
 
 const handleRejected = (state, action) => {
@@ -18,27 +19,27 @@ const handleRejected = (state, action) => {
 
 export const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: { items: [], loading: false, error: null },
+  initialState: { items: [], loading: false, error: '' },
   extraReducers: builder => {
     builder
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.loading = false;
-        state.error = null;
+        state.error = '';
         state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, handleRejected)
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.error = null;
+        state.error = '';
         state.items.push(action.payload);
       })
       .addCase(addContact.rejected, handleRejected)
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.error = null;
+        state.error = '';
         const index = state.items.findIndex(
           contact => contact.id === action.payload.id
         );
@@ -48,7 +49,7 @@ export const contactsSlice = createSlice({
       .addCase(editContact.pending, handlePending)
       .addCase(editContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.error = false;
+        state.error = '';
         const index = state.items.findIndex(
           contact => contact.id === action.payload.id
         );
@@ -57,7 +58,7 @@ export const contactsSlice = createSlice({
       .addCase(logout.fulfilled, state => {
         state.items = [];
         state.loading = false;
-        state.error = null;
+        state.error = '';
       });
   },
 });
