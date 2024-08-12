3378fac8ec8db7248a292afb7bede38c991eb95b
Yura Kasyan <33.yurakasyan@gmail.com>
2024-08-12 21:28:00 +0200

    refactor: makes changes in ContactsPage component

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,15 +1,24 @@
 import { useContext, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
-import { selectFilteredContacts } from '../../redux/contacts/selectors';
 import { fetchContacts } from '../../redux/contacts/operations';
-import { ModalWindowContext } from '../../context/ModalWindow/modal.context';
-import ContactForm from '../../components/ContactForm/ContactForm';
-import ContactCard from '../../components/ContactCard/ContactCard';
+import { selectLoading } from '../../redux/contacts/selectors';
+import { ModalWindowContext } from '../../helpers/context/modal.context';
+import {
+  MODAL_DELETE_CONTACT,
+  MODAL_EDIT_CONTACT,
+  MODAL_NEW_CONTACT,
+} from '../../helpers/constants/modalConstants';
+import ContactNewForm from '../../components/ContactNewForm/ContactNewForm';
 import ModalWindow from '../../components/ui/ModalWindow/ModalWindow';
+import ContactList from '../../components/ContactList/ContactList';
+import ContactEditForm from '../../components/ContactEditForm/ContactEditForm';
+import ContactDeleteForm from '../../components/ContactDeleteForm/ContactDeleteForm';
+import TypographyHeader from '../../components/ui/TypographyHeader/TypographyHeader';
 
-import { Button, List, ListItem, Typography } from '@mui/material';
+import { Button } from '@mui/material';
 import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
+import Filter from '../../components/Filter/Filter';
 
 function ContactsPage() {
   const dispatch = useDispatch();
@@ -18,54 +27,38 @@ function ContactsPage() {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const contacts = useSelector(selectFilteredContacts);
-  const { handleOpen } = useContext(ModalWindowContext);
+  const { isLoading } = useSelector(selectLoading);
+  const { handleModalOpen } = useContext(ModalWindowContext);
 
   return (
     <>
-      <Typography
+      <TypographyHeader
         variant="h2"
-        sx={{ my: 2, fontSize: { xs: 28, sm: 40, md: 48 } }}
-      >
-        The list of contacts
-      </Typography>
-
-      <List
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', sm: 'row' },
-          gap: { xs: '15px' },
-          flexWrap: 'wrap',
-        }}
-      >
-        {contacts.map(contact => (
-          <ListItem
-            sx={{
-              overflow: 'hidden',
-              padding: '0',
-              flex: {
-                sm: '1 1 calc(50% - 30px)',
-                md: '1 1 calc(33.33% - 45px)',
-              },
-            }}
-            key={contact.id}
-          >
-            <ContactCard name={contact.name} number={contact.number} />
-          </ListItem>
-        ))}
-      </List>
-
+        title="The list of contacts"
+        styles={{ my: 2, fontSize: { xs: 28, sm: 40, md: 48 } }}
+      />
+      <Filter />
+      <ContactList />
       <Button
         variant="contained"
         startIcon={<PersonAddAlt1OutlinedIcon />}
         sx={{ display: 'flex', margin: '15px auto' }}
-        onClick={handleOpen}
+        onClick={() => handleModalOpen(MODAL_NEW_CONTACT)}
+        disabled={isLoading}
       >
         new contact
       </Button>
 
-      <ModalWindow title="New contact">
-        <ContactForm />
+      <ModalWindow title="New" modalType={MODAL_NEW_CONTACT}>
+        <ContactNewForm />
+      </ModalWindow>
+
+      <ModalWindow title="Edit" modalType={MODAL_EDIT_CONTACT}>
+        <ContactEditForm />
+      </ModalWindow>
+
+      <ModalWindow title="Delete" modalType={MODAL_DELETE_CONTACT}>
+        <ContactDeleteForm />
       </ModalWindow>
 
       <Toaster />
