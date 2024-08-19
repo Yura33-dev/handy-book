9408c8af406aa7faab32117e34d978a5882cfd92
Yura Kasyan <33.yurakasyan@gmail.com>
2024-08-19 18:09:11 +0200

    chore: adds typescript to project

--- src/main.tsx
+++ src/main.tsx
@@ -45,7 +45,7 @@ const theme = createTheme({
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
