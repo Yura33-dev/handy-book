6874ff14383620de89b85598bd179ccf26c3ffe5
Yura Kasyan <33.yurakasyan@gmail.com>
2024-08-27 13:01:18 +0200

    refactor: improves modal windows Context

--- src/helpers/context/modal.context.tsx
+++ src/helpers/context/modal.context.tsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from 'react';
 import { IModal, IModalsContext } from './modalTypes';
+import { ModalConstants } from '../constants/modalConstants';
 
 const defaultContext: IModalsContext = {
   isModalOpen: () => false,
@@ -14,10 +15,20 @@ type ModalProviderProps = {
 
 export const ModalWindowContext = createContext<IModalsContext>(defaultContext);
 
+const initialModalsState = Object.values(ModalConstants).reduce(
+  (accObj, currentModalName) => {
+    accObj[currentModalName] = {
+      isOpen: false,
+      data: null,
+    };
+
+    return accObj;
+  },
+  {} as IModal
+);
+
 export function ModalProvider({ children }: ModalProviderProps) {
-  const [modals, setModals] = useState<IModal>({
-    default: { isOpen: false, data: null },
-  });
+  const [modals, setModals] = useState<IModal>(initialModalsState);
 
   const handleModalOpen = (modalName: string, data: string | null = null) => {
     setModals(state => ({ ...state, [modalName]: { isOpen: true, data } }));
